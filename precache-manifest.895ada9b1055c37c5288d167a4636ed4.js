8f06b7a43ea821955a871b29739952d8eeb8acb9
fito <fifodominguez@gmail.com>
2019-06-29 19:49:37 -0600

    Updates

---
+++ precache-manifest.895ada9b1055c37c5288d167a4636ed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5531377014e45ef3902e746f2a46a0",
+    "url": "/climareact/index.html"
+  },
+  {
+    "revision": "cb275bc3b9fc8de33885",
+    "url": "/climareact/static/css/main.3751ae63.chunk.css"
+  },
+  {
+    "revision": "4f245731a1061defc317",
+    "url": "/climareact/static/js/2.28f1cc8d.chunk.js"
+  },
+  {
+    "revision": "cb275bc3b9fc8de33885",
+    "url": "/climareact/static/js/main.f8771a5f.chunk.js"
+  },
+  {
+    "revision": "7e06bb4efedc56364e9c",
+    "url": "/climareact/static/js/runtime~main.9a0139d1.js"
+  }
+]);
